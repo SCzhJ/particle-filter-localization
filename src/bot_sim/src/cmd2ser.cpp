26103df4847e5bb8db21d4d2b77c1e8fbd360872
Zhang Shengce <shengcezhang@163.com>
2024-01-25 13:31:54 +0800

    added real robot mapping

---
+++ src/bot_sim/src/cmd2ser.cpp
@@ -0,0 +1,105 @@
+#include <ros/ros.h>
+#include <geometry_msgs/Twist.h>
+#include <serial/serial.h>
+#include <algorithm>
+#include <string>
+
+serial::Serial ser;
+
+union FloatToByte{
+    float f;
+    uint8_t bytes[sizeof(float)];
+};
+
+void cmdVelCallback(const geometry_msgs::Twist::ConstPtr &msg)
+{
+    uint8_t buffer[13];
+
+    // Start of Frame
+    buffer[0] = 0xA3; // SOF
+
+    // Linear velocities x
+    FloatToByte linear_x;
+    linear_x.f = msg->linear.x;
+    std::copy(std::begin(linear_x.bytes),std::end(linear_x.bytes),&buffer[1]);
+
+    // Linear velocities y
+    FloatToByte linear_y;
+    linear_y.f = msg->linear.y;
+    std::copy(std::begin(linear_y.bytes),std::end(linear_y.bytes),&buffer[5]);
+
+    // Angular velocity
+    FloatToByte angular_z;
+    angular_z.f = msg->angular.z;
+    std::copy(std::begin(angular_z.bytes),std::end(angular_z.bytes),&buffer[9]);
+
+    // Write to the serial port
+    // ser.write(0xA3, 1); // test message
+    size_t bytes_written = ser.write(buffer,13);
+    if (bytes_written < 13){
+        ROS_ERROR("Failed to write all bytes to the serial port");
+    }
+
+    // // Logging buffer content for debug
+    // std::stringstream ss;
+    // for(int i=0;i<13;i++){
+    //     ss << "0x" << std::hex << std::setw(2) << std::setfill('0') << static_cast<int>(buffer[i]) << " ";
+    // }
+    // ROS_INFO_STREAM("Buffer content: " << ss.str());
+
+}
+
+int main(int argc, char **argv)
+{
+    std::string node_name = "cmd2ser";
+    ros::init(argc, argv, node_name);
+    ros::NodeHandle nh;
+
+    std::string base_serial_port;
+    if (nh.getParam("/"+node_name+"/base_serial_port",base_serial_port)){
+		ROS_INFO("Got param: %s", base_serial_port.c_str());
+    }
+    else{
+    	ROS_ERROR("Failed to get param: %s", base_serial_port.c_str());
+    }
+
+    std::string vel_topic;
+    if (nh.getParam("/"+node_name+"/vel_topic",vel_topic)){
+		ROS_INFO("Got param: %s", vel_topic.c_str());
+    }
+    else{
+    	ROS_ERROR("Failed to get param: %s", vel_topic.c_str());
+    }
+
+    try{
+	    // Configuration
+    	ser.setPort(base_serial_port);
+    	ser.setBaudrate(9600);
+    	serial::Timeout to = serial::Timeout::simpleTimeout(1000);
+    	ser.setTimeout(to);
+		ser.setBytesize(serial::eightbits);
+		ser.setStopbits(serial::stopbits_one);
+    	ser.setParity(serial::parity_none);
+		ser.setFlowcontrol(serial::flowcontrol_none);
+
+	    // open port
+    	ser.open();
+    }
+    catch(serial::IOException& e){
+		ROS_ERROR_STREAM("Unable to open port");
+		return -1;
+    }
+
+    if(ser.isOpen()){
+		ROS_INFO_STREAM("Serial Port initialized");
+    }
+    else{
+		return -1;
+    }
+
+    ros::Subscriber sub = nh.subscribe(vel_topic, 1000, cmdVelCallback);
+
+    ros::spin();
+
+    return 0;
+}
---
+++ src/bot_sim/src/real_robot_transform.cpp
@@ -0,0 +1,44 @@
+#include <ros/ros.h>
+#include <tf2_ros/transform_broadcaster.h>
+#include <geometry_msgs/TransformStamped.h>
+
+int main(int argc, char** argv){
+    ros::init(argc, argv, "robot_tf_publisher");
+    ros::NodeHandle n;
+
+    tf2_ros::TransformBroadcaster broadcaster;
+
+    geometry_msgs::TransformStamped transformStamped1;
+    transformStamped1.header.frame_id = "aft_mapped";
+    transformStamped1.child_frame_id = "base_footprint";
+    transformStamped1.transform.translation.x = 0.0;
+    transformStamped1.transform.translation.y = -0.234;
+    transformStamped1.transform.translation.z = -0.4;
+    transformStamped1.transform.rotation.x = 0;
+    transformStamped1.transform.rotation.y = 0;
+    transformStamped1.transform.rotation.z = sqrt(2)/2;
+    transformStamped1.transform.rotation.w = sqrt(2)/2;
+
+    geometry_msgs::TransformStamped transformStamped2;
+    transformStamped2.header.frame_id = "base_footprint";
+    transformStamped2.child_frame_id = "laser";
+    transformStamped2.transform.translation.x = 0.234;
+    transformStamped2.transform.translation.y = 0.0;
+    transformStamped2.transform.translation.z = 0.3;
+    transformStamped2.transform.rotation.x = 0;
+    transformStamped2.transform.rotation.y = 0;
+    transformStamped2.transform.rotation.z = 1;
+    transformStamped2.transform.rotation.w = 0;
+
+    ros::Rate rate(20.0); // 10 Hz
+    while (n.ok()){
+        transformStamped1.header.stamp = ros::Time::now();
+        transformStamped2.header.stamp = ros::Time::now();
+
+        broadcaster.sendTransform(transformStamped1);
+        broadcaster.sendTransform(transformStamped2);
+        ros::spinOnce();
+        rate.sleep();
+    }
+    return 0;
+}
