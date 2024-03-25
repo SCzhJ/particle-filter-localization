77c34b49241805e214d1a96974981514228f032c
Zhang Shengce <shengcezhang@163.com>
2024-03-25 16:26:50 +0800

    watchdog

--- src/bot_sim/scripts/temp/node_watcher.py
+++ src/bot_sim/scripts/temp/node_watcher.py
@@ -23,62 +23,90 @@ if __name__ == '__main__':
             pass
         else:
             subprocess.Popen(['roslaunch', 'decision', 'decision_serial.launch'])
+            time.sleep(1)
 
         if is_node_alive('decision_node'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'decision', 'decision_node.launch'])
+            time.sleep(1)
 
-        #ser2msg
-        if is_node_alive('ser2msg'):
+        #real_robot
+        if is_node_alive('real_robot_transform'):
             pass
         else:
-            subprocess.Popen(['roslaunch', 'bot_sim', 'ser2msg_write.launch'])
+            subprocess.Popen(['roslaunch', 'bot_sim', 'real_robot_transform.launch'])
+            time.sleep(1)
         
         #lidar
         if is_node_alive('livox_lidar_publisher2'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'livox_ros_driver2', 'msg_mixed.launch'])
+            time.sleep(1)
         
         #lidar_filter
         if is_node_alive('threeD_lidar_filter'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'bot_sim', 'lidar_filter.launch'])
+            time.sleep(1)
         
         #3D22D
         if is_node_alive('pointcloud_to_laserscan'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'bot_sim', '3D22D.launch'])
+            time.sleep(1)
+        
+        if is_node_alive('lidar_monitor'):
+            pass
+        else:
+            subprocess.Popen(['roslaunch', 'bot_sim', 'LidarMonitor.launch'])
+            time.sleep(1)
 
         #dbscan_bfs_3D
         if is_node_alive('dbscan_bfs_3D'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'bot_sim', 'dbscan_bfs_3D.launch'])
+            time.sleep(1)
         
         #point_lio
         if is_node_alive('laserMapping'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'point_lio', 'mapping_avia.launch'])
+            time.sleep(1)
 
         #amcl
         if is_node_alive('amcl'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'bot_sim', 'launch_amcl_real.launch'])
-        #navigate_ctrl
-        if is_node_alive('nav_ctrl'):
+            time.sleep(1)
+
+        #ser2msg
+        if is_node_alive('ser2msg_write_only'):
             pass
         else:
-            subprocess.Popen(['roslaunch', 'bot_sim', 'navigate_ctrl.launch'])
-        
+            subprocess.Popen(['roslaunch', 'bot_sim', 'ser2msg_tf_write.launch'])
+            time.sleep(1)
+
         #rrtstar
         if is_node_alive('RRT_star_srv'):
             pass
         else:
             subprocess.Popen(['roslaunch', 'bot_sim', 'rrtstar.launch'])
-        time.sleep(0.01)
+            time.sleep(1)
+        
+        #navigate_ctrl
+        if is_node_alive('nav_ctrl'):
+            pass
+        else:
+            subprocess.Popen(['roslaunch', 'bot_sim', 'navigate_ctrl.launch'])
+            time.sleep(1)
+        
+        
+        
+        time.sleep(10)
